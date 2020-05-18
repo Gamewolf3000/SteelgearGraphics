d30457c7facce3ca9afde4d5257602e4187bcc5c
Joakim Ståhle Nilsson <jzn@bth.se>
2020-05-18 17:07:53 +0200

    Started working on the buffer handler in D3D11

--- SteelgearGraphics/SteelgearGraphics/D3D11BufferHandler.h
+++ SteelgearGraphics/SteelgearGraphics/D3D11BufferHandler.h
@@ -28,12 +28,30 @@ namespace SG
 		D3D11BufferHandler(ID3D11Device* device);
 		~D3D11BufferHandler();
 
-		SGError CreateVertexBuffer(const SGGraphicalEntityID& entity, const SGGuid& guid, UINT size, bool dynamic, bool streamout, void* data);
-		SGError CreateGlobalVertexBuffer(const SGGuid& guid, UINT size, bool dynamic, bool streamout, void* data);
+		SGError CreateVertexBuffer(const SGGraphicalEntityID& entity, const SGGuid& guid, UINT size, bool dynamic, bool streamOut, void* data);
+		SGError CreateGlobalVertexBuffer(const SGGuid& guid, UINT size, bool dynamic, bool streamOut, void* data);
 		SGError CreateIndexBuffer(const SGGraphicalEntityID& entity, const SGGuid& guid, UINT size, bool dynamic, void* data);
 		SGError CreateGlobalIndexBuffer(const SGGuid& guid, UINT size, bool dynamic, void* data);
+		SGError CreateConstantBuffer(const SGGraphicalEntityID& entity, const SGGuid& guid, UINT size, bool dynamic, bool cpuUpdate, void* data);
+		SGError CreateGlobalConstantBuffer(const SGGuid& guid, UINT size, bool dynamic, bool cpuUpdate, void* data);
+		SGError CreateStructuredBuffer(const SGGraphicalEntityID& entity, const SGGuid& guid, UINT count, UINT structSize, bool cpuWritable, bool gpuWritable, void* data);
+		SGError CreateGlobalStructuredBuffer(const SGGuid& guid, UINT count, UINT structSize, bool cpuWritable, bool gpuWritable, void* data);
+
+
 
 		SGError CreateBuffer(const SGGraphicalEntityID& entity, const SGGuid& guid);
+
+		/**
+			entityGuid is the BufferData of the entity where the srv is to be stored
+			bufferGuid is the BufferData to create a SRV for. First look in the entity, then if not found, look among global buffers
+		*/
+		SGError CreateSRV(const SGGraphicalEntityID& entity, const SGGuid& entityGuid, const SGGuid& bufferGuid, UINT firstElement, UINT numberOfElements);
+		/**
+			Creates a srv for a global buffer. It is not connected to any entity
+		*/
+		SGError CreateGlobalSRV(const SGGuid& bufferGuid, UINT firstElement, UINT numberOfElements);
+
+
 
 
 	private:
@@ -41,9 +59,11 @@ namespace SG
 		struct BufferData
 		{
 			BufferType type;
+			bool updated = false;
 			ID3D11Buffer* buffer = nullptr;
 			ID3D11ShaderResourceView* srv = nullptr;
 			ID3D11UnorderedAccessView* uav = nullptr;
+			void* UpdatedData[3] = { nullptr, nullptr, nullptr };
 		};
 
 		std::vector<std::unordered_map<SGGuid, BufferData>> entityBufferData;
